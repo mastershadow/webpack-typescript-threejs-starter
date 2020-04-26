e50224d9528b7b052169e5e15f7a021822a700d1
Eduard Roccatello <eduard.roccatello@3dgis.it>
2020-04-26 16:39:53 +0200

    naming

--- src/sample/Sample.ts
+++ src/sample/Sample.ts
@@ -3,18 +3,18 @@ import { Cube } from './Cube';
 import { Color } from 'three';
 
 export class Sample extends CoreApp {
-    private brick!: Cube;
+    private cube!: Cube;
 
     // tslint:disable-next-line: no-empty
     protected beforeInit() {
     }
 
     protected afterInit() {
-        this.brick = new Cube(100, new Color('rgb(255,0,0)'));
-        this.scene.add(this.brick);
+        this.cube = new Cube(100, new Color('rgb(255,0,0)'));
+        this.scene.add(this.cube);
     }
 
     protected update() {
-        this.brick.rotateY(0.03);
+        this.cube.rotateY(0.01);
     }
 }
